3300fea02f0c756ff438fe038ec58b97a8389313
Danil_Bespalov <dbaservice@yandex.ru>
2023-04-09 23:37:32 +0300

    create, edit, delete

--- src/main/resources/static/code.js
+++ src/main/resources/static/code.js
@@ -9,44 +9,49 @@ const url = 'http://localhost:8080/api/admin'
 const container = document.querySelector('tbody')
 let resultData = ''
 
-const modalNew = new bootstrap.Modal(document.getElementById('modalNew'))
-const formEdit = document.querySelector('.form')
+// const modalNew = new bootstrap.Modal(document.getElementById('modalNew'))
+const formCreate = document.querySelector('.form')
+const id = document.getElementById('id')
 const userName = document.getElementById('username')
-const nameCreate = document.getElementById('name')
-const lastName = document.getElementById('surname')
+const name = document.getElementById('name')
+const surname = document.getElementById('surname')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
-const selectRole = document.getElementById('roles')
+const roles = document.getElementById('roles')
 let option = ''
 
-// Edit button event
+// Create button event
 btnCreate.addEventListener('click', ()=>{
-    // idUpdate.value = ''
+    id.value = ''
     userName.value = ''
-    nameCreate.value = ''
-    lastName.value = ''
+    name.value = ''
+    surname.value = ''
     email.value = ''
     password.value = ''
-    selectRole.value = ''
+    roles.value = ''
     modalNew.show()
-    option = "create"
+    option = 'create'
 })
 
 //function dataShow
 const dataShow = (elements) => {
     elements.forEach(element => {
+
+        const rolesName = element.roles.map(role => role.role.replace('ROLE_', ' ')).join(', ')
         resultData +=`
     <tr>
-        <td>${element.userName}</td>
-        <td>${element.nameCreate}</td>
-        <td>${element.lastName}</td>
+        <td>${element.id}</td>
+        <td>${element.username}</td>
+        <td>${element.name}</td>
+        <td>${element.surname}</td>
         <td>${element.email}</td>
-        <td>${element.password}</td>
-        <td class="text-center">
+        <td>${rolesName}</td>
+        <td>
             <a class="btnEdit btn btn-primary">Edit</a>
+            </td>
+        <td>
             <a class="btnDelete btn btn-danger">Delete</a>
             </td>
-        <td>${element.selectRole}</td>
     </tr>`
     });
     container.innerHTML = resultData
@@ -56,5 +61,5 @@ const dataShow = (elements) => {
 fetch(url)
     .then(response => response.json())
     .then(data => dataShow(data))
-    .catch(eror => console.log(eror))
+    .catch(error => console.log(error))
 
