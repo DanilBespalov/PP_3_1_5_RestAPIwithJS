e59fc450c593ed80c66143908467f17c366c8c05
Danil_Bespalov <dbaservice@yandex.ru>
2023-04-27 23:14:33 +0300

    Create new user / roles

--- src/main/resources/static/newUser.js
+++ src/main/resources/static/newUser.js
@@ -1,28 +1,42 @@
-async function showRole() {
+// // Функция для получения ролей из API
+// async function showAllRole() {
+//     let dbRoles = [];
+//     let roles = await fetch("http://localhost:8080/api/admin/roles"); 
+//     await roles.json().then(roles => {
+//         roles.forEach(role =>
+//             dbRoles.push(role))
+//     });
+//     return dbRoles;
+// }
+// showAllRole().then(console.log)
 
-    const inputRoles = document.getElementById('rolesNew');
-    let dbRoles = await showAllRole();
-    inputRoles.innerHTML = `
-            <option value="${dbRoles[0].id}">${dbRoles[0].name}</option>
-            <option value="${dbRoles[1].id}">${dbRoles[1].name}</option>
-            `
-}
 
-document.getElementById('profile-tab').addEventListener('click', showRole)
+// async function showRole() {
 
-const rolesSelect = document.getElementById('rolesNew');
-// Запрашиваем список ролей с сервера и преобразуем его в элементы option
-fetch('http://localhost:8080/api/admin/roles')
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(role => {
-      const option = document.createElement('option');
-      option.value = role.id;
-      option.text = role.name;
-      rolesSelect.appendChild(option);
-    });
-  });
+//     const inputRoles = document.getElementById('rolesNew');
+//     let dbRoles = await showAllRole();
+//     inputRoles.innerHTML = `
+//             <option value="${dbRoles[0].id}">${dbRoles[0].name}</option>
+//             <option value="${dbRoles[1].id}">${dbRoles[1].name}</option>
+//             `
+// }
 
+
+// document.getElementById('profile-tab').addEventListener('click', showRole)
+
+// const rolesSelect = document.getElementById('rolesNew');
+// // Запрашиваем список ролей с сервера и преобразуем его в элементы option
+// fetch('http://localhost:8080/api/admin/roles')
+//   .then(response => response.json())
+//   .then(data => {
+//     data.forEach(role => {
+//       const option = document.createElement('option');
+//       option.value = role.id;
+//       option.text = role.name;
+//       rolesSelect.appendChild(option);
+//     });
+//   });
+  
 
 // Находим ссылку, форму и модальное окно с кнопкой createButton
 const createLink = document.querySelector('#addNewUser');
@@ -38,22 +52,27 @@ createLink.addEventListener('click', (event) => {
     createForm.style.display = 'block';       // Отображаем форму
 });
 
-
+// Добавляем обработчик событий на клик по кнопке создания пользователя
+createForm.addEventListener('submit', event => {
+    event.preventDefault();
+    // showRole();
+  });
+  
 // Добавляем обработчик событий на клик по кнопке создания пользователя
 createButton.addEventListener('click', createUser)
 async function createUser() {
-
+    
     const inputUserName = document.getElementById('usernameNew').value
     const inputName = document.getElementById('nameNew').value
     const inputSurname = document.getElementById('surnameNew').value
     const inputEmail = document.getElementById('emailNew').value
     const inputPassword = document.getElementById('passwordNew').value
 
-    let listRoles = await roleArray(document.getElementById('rolesNew'));
+    // let listRoles = await roleArray(document.getElementById('rolesNew'));
 
     
-    console.log(inputUserName, inputName, inputSurname, inputEmail, inputPassword, listRoles);
-    if (inputUserName && inputName && inputSurname && inputEmail && inputPassword && listRoles) {
+    console.log(inputUserName, inputName, inputSurname, inputEmail, inputPassword);
+    if (inputUserName && inputName && inputSurname && inputEmail && inputPassword) {
 
         let user = {
             username: inputUserName,
@@ -61,7 +80,7 @@ async function createUser() {
             surname: inputSurname, 
             email: inputEmail,
             password: inputPassword,
-            roles: listRoles
+            // roles: listRoles
           };
 
         let res = await fetch(`http://localhost:8080/api/admin/new`, {
@@ -75,10 +94,10 @@ async function createUser() {
         console.log("  пользователь получен")
         const result = await res.json();             // Получаем ответ от сервера в формате JSON
         console.log("создан пользователь: ", result);
-        await list()                                 // Обновляем список пользователей
         $('#nav-tab-home').click();                  // Переключаемся на вкладку Home
     }
 
     // document.getElementById('form-create').reset();
 
 }
+
